82fbd421ad669420d6b11a3974a10ef789a3719a
drunque <lufi.2023@gmail.com>
2023-04-24 17:20:43 +0300

    updated calculator

--- calculator/script.js
+++ calculator/script.js
@@ -1,41 +1,22 @@
-function calc(a, b, operation) {
-  const operations = {
-    add: a + b,
-    sub: a - b,
-    mul: a * b,
-    div: a / b,
-  };
-
-  const validation = {
-    isValidOperation: Object.keys(operations).includes(operation),
-
-    isValidNumbers(...numbers) {
-      return numbers.filter(Number.isFinite).length === numbers.length;
-    },
-  };
-
-  if (validation.isValidNumbers(a, b) && validation.isValidOperation) {
-    let result = operations[operation];
-
-    if (!Number.isFinite(result)) return "Error";
-    
-    if (!Number.isInteger(result)) {
-      result = result.toFixed(2);
-    }
-
-    return result.toString().slice(0, 30);
-  }
-  return "Error";
-}
+import { calc } from "./calc.js";
 
 const resultButton = document.querySelector("#result-button");
+const results = document.querySelector(".results");
 resultButton.addEventListener("click", () => {
-  const numberNodes = Array.from(document.querySelectorAll("input"));
-  const numbers = numberNodes.map((node) => Number.parseFloat(node.value) );
-
+  const numbers = Array.from(document.querySelectorAll("input")).map(
+    (node) => Number.parseFloat(node.value)
+  );
   const operation = document.querySelector(".current div").dataset.value;
   const result = calc(...numbers, operation);
   const resultNode = document.querySelector("#result");
 
-  if (result) resultNode.textContent = result;
+  if (result) resultNode.textContent = result.value;
+
+  const historyResult = document.createElement("div");
+  historyResult.classList.add("rectangle");
+  historyResult.textContent = `${numbers[0]} ${result.sign} ${numbers[1]} = ${result.value}`;
+  results.appendChild(historyResult);
+  results.scrollTop = results.scrollHeight;
+
+  historyResult.addEventListener("click", historyResult.remove);
 });
